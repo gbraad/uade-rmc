daeff63c18c45d8c8340690c242f8ef21cc1e9ea
Heikki Orsila <heikki.orsila@iki.fi>
2011-03-06 14:40:30 +0200

    rmc: Implement option parsing [PERFECTIVE]

--- rmc.c
+++ rmc.c
@@ -1,6 +1,28 @@
+#include "utils.h"
+
 #include <uade/uade.h>
+#include <getopt.h>
 
 int main(int argc, char *argv[])
 {
+	int ret;
+	int i;
+
+	while (1) {
+		ret = getopt(argc, argv, "d");
+		if (ret  < 0)
+			break;
+		switch (ret) {
+		case 'd':
+			die("-d not implemented\n");
+		default:
+			die("Unknown option: %c\n", optopt);
+		}
+	}
+
+	for (i = optind; i < argc; i++) {
+		debug("Process %s\n", argv[i]);
+	}
+
 	return 0;
 }
---
+++ utils.h
@@ -0,0 +1,11 @@
+#ifndef _UADE_UNIXSUPPORT_H_
+#define _UADE_UNIXSUPPORT_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define debug(fmt, args...) do { fprintf(stderr, fmt, ## args); } while (0)
+#define die(fmt, args...) do { fprintf(stderr, "rmc error: " fmt, ## args); exit(1); } while(0)
+#define warning(fmt, args...) do { fprintf(stderr, "rmc warning: " fmt, ## args); } while(0)
+
+#endif
