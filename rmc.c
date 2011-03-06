bceeeb8c7bdc9b95efea685b0f448fc31b66d939
Heikki Orsila <heikki.orsila@iki.fi>
2011-03-06 15:02:06 +0200

    rmc: Start implementing amiga song to rmc conversion [PERFECTIVE]

--- rmc.c
+++ rmc.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
 {
 	int ret;
 	int i;
+	struct uade_state *state = NULL;
 
 	while (1) {
 		ret = getopt(argc, argv, "d");
@@ -21,8 +22,34 @@ int main(int argc, char *argv[])
 	}
 
 	for (i = optind; i < argc; i++) {
+		struct uade_file *f = uade_file_load(argv[i]);
+		if (f == NULL) {
+			fprintf(stderr, "Can not open %s\n", argv[i]);
+			continue;
+		}
 		debug("Process %s\n", argv[i]);
+
+		if (state == NULL)
+			state = uade_new_state(NULL, NULL);
+		if (state == NULL)
+			die("Can not initialize uade state\n");
+
+		ret = uade_play_from_buffer(f->name, f->data, f->size, -1, state);
+		if (ret < 0) {
+			uade_cleanup_state(state);
+			state = NULL;
+			warning("Fatal error in uade state when initializing %s\n", argv[i]);
+			goto nextfile;
+		} else if (ret == 0) {
+			debug("%s is not playable\n", argv[i]);
+			goto nextfile;
+		}
+
+	nextfile:
+		uade_file_free(f);
 	}
 
+	/* state can be NULL */
+	uade_cleanup_state(state);
 	return 0;
 }
