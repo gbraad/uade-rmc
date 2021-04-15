bba9928561649056e009547ae0e69d64e4646573
Heikki Orsila <heikki.orsila@iki.fi>
2021-04-15 16:02:45 +0300

    Force import dependencies when import-dependencies.sh is run [PERFECTIVE].

--- import-dependencies.sh
+++ import-dependencies.sh
@@ -3,21 +3,20 @@
 libzakalweurl="https://gitlab.com/hors/libzakalwe"
 libzakalwehead="ac84680f86aea88c9219d6be7482916e83fff0c7"
 
-if [[ ! -e libzakalwe ]] ; then
-    while true ; do
-	echo -n "Do you trust to import ${libzakalweurl} ${libzakalwehead}? (y/n) "
-	read answer
-	if [[ ${answer} = "y" ]] ; then
-	    break;
-	elif [[ ${answer} = "n" ]] ; then
-	    echo "Abort"
-	    exit 1
-	fi
-    done
-    git clone -n ${libzakalweurl} && cd libzakalwe && git reset --hard ${libzakalwehead}
-    if [[ $? != "0" ]] ; then
-	echo "Clone failed"
-	cd ..
-	rm -rf libzakalwe
+rm -rf libzakalwe
+while true ; do
+    echo -n "Do you trust to import ${libzakalweurl} ${libzakalwehead}? (y/n) "
+    read answer
+    if [[ ${answer} = "y" ]] ; then
+        break;
+    elif [[ ${answer} = "n" ]] ; then
+	echo "Abort"
+	exit 1
     fi
+done
+git clone -n ${libzakalweurl} && cd libzakalwe && git reset --hard ${libzakalwehead}
+if [[ $? != "0" ]] ; then
+    echo "Clone failed"
+    cd ..
+    rm -rf libzakalwe
 fi
